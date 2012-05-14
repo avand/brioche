b7083a2dee89d895ff394bb986ca5e80bc58a5b0
Avand Amiri <avand@avandamiri.com>
2012-05-14 12:20:01 -0700

    Initial commit

---
+++ server.rb
@@ -0,0 +1,21 @@
+require "sinatra"
+require "google_drive"
+
+post "/expenses" do
+  amount      = params[:amount]
+  description = params[:description]
+  date        = Time.now.strftime("%-m/%-d/%Y")
+
+  session     = GoogleDrive.login(ENV["GOOGLE_EMAIL"], ENV["GOOGLE_PASSWORD"])
+  spreadsheet = session.spreadsheet_by_key("0AkvbHgDD4uKFdGJ0N3RsWHNiaGRnYk5hdlp3ZEZVaWc")
+  worksheet   = spreadsheet.worksheets[1]
+  row         = worksheet.num_rows + 1
+
+  worksheet[row, 1] = description
+  worksheet[row, 2] = date
+  worksheet[row, 3] = amount
+
+  worksheet.save
+
+  "Success!"
+end
